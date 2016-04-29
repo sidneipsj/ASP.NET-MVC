c183d3b0d6e5f31a31454ad8b478ada4c1773126
Sidnei Sanches <sidneipsj@gmail.com>
2016-04-29 14:08:09 -0300

    aDICIONADO PROJETOS ENTITY FRAMEWORK - DEVMEDIA CRIANDO UM Blog - AULAS DO CURSO 1935 ENTITY FRAMEWORK ANDRE BALTIERI(TODAS AS DEMOS) - CODIGO DO BLOG MODERADO PELA DEVMEDIA

---
+++ EntityFrameworkProjects/DevMedia.Forum/DevMedia.Forum/Controllers/LoginController.cs
@@ -0,0 +1,54 @@
+﻿using DevMedia.Forum.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevMedia.Forum.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private usuario autenticarUsuario;
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(String login, String senha)
+        {
+            if (autenticarUsuario != null)
+            {
+                Session.Add("usuario", autenticarUsuario);
+                return Redirect("/Posts/Index");
+            }
+            else
+            {
+                ViewBag.ErroAutenticacao = "Usuário ou senha inválidos.";
+                return View("Index");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CadastrarUsuario(usuario usuario)
+        {
+
+            BD_FORUMEntities ctx = new BD_FORUMEntities();
+
+            var senhaCriptografada = FormsAuthentication.HashPasswordForStoringInConfigFile(usuario.senha, "MD5");
+
+            usuario.senha = senhaCriptografada;
+            usuario.data_cadastro = DateTime.Now;
+            usuario.adm = "N";
+            ctx.usuario.Add(usuario);
+            ctx.SaveChanges();
+
+            ViewBag.StatusCadastro = "Cadastro realizado com sucesso.";
+
+            return View("Index");
+        }
+    }
+}
---
+++ EntityFrameworkProjects/DevMedia.Forum/DevMedia.Forum/Repositorio/VerificaUsuarioLogado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevMedia.Forum.Repositorio
+{
+    public class VerificaUsuarioLogado : FilterAttribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Boolean usuarioLogado = AutenticarUsuario.verificarSessaoUsuario();
+
+            if (!usuarioLogado)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                 new RouteValueDictionary(
+                     new { action = "Index", Controller = "Login", area = "" }));
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+        }
+    }
+}
