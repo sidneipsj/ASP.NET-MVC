7e3b12fa337548e462f8a195dc57f3244b64f137
Sidnei Sanches <sidneipsj@gmail.com>
2016-06-15 15:34:09 -0300

    Adicionei o projeto: - MODAL LOGIN FORM - MODEL_STATE_VALIDATE

---
+++ MODEL_STATE_VALIDATE FORM/Mvc_ModelState/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using Mvc_ModelState.Models;
+
+namespace Mvc_ModelState.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Incluir()
+        {
+            Usuario model = new Usuario();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Incluir(Usuario model)
+        {
+            if (model.Nome == model.SobreNome)
+            {
+                ModelState.AddModelError("Sobrenome", "O sobrenome não pode ser igual ao nome.");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MODEL_STATE_VALIDATE FORM/Mvc_ModelState/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mvc_ModelState.Models
+{
+    public class Usuario
+    {
+        [Required(ErrorMessage = "Informe o nome do usuário.")]
+        [StringLength(50, ErrorMessage = "O nome não deve exceder {1} caracteres.")]
+        [Display(Name = "Nome do Usuario:")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe o sobrenome do usuário.")]
+        [StringLength(50, ErrorMessage = "O sobrenome não deve exceder {1} caracteres.")]
+        [Display(Name = "Sobrenome do Usuário:")]
+        public string SobreNome { get; set; }
+
+        [EmailAddress(ErrorMessage = "O endereço de email não é válido")]
+        [Required(ErrorMessage = "Informe um endereço de email.")]
+        [Display(Name = "Endereço de Email:")]
+        public string Email { get; set; }
+    }
+}
